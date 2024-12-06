effd2c2529b8b4305a0803786f74b0db90953e0f
yakov <yakovrussian@gmail.com>
2024-12-06 22:43:59 +0100

    feat: apply sort + filter in reactive manner

--- common/currencies/api/src/commonMain/kotlin/presentation/decompose/CurrencyListComponent.kt
+++ common/currencies/api/src/commonMain/kotlin/presentation/decompose/CurrencyListComponent.kt
@@ -13,10 +13,17 @@ interface CurrencyListComponent {
     fun onBackClick()
     fun changeSortOrder(newSortOption: SortOption)
 
-    fun List<CurrencyEntity>?.sortedBySearchText(): List<CurrencyEntity>? = this?.filter { cur ->
+    fun List<CurrencyEntity>?.filteredBySearch(): List<CurrencyEntity>? = this?.filter { cur ->
         val searchText = screenState.value.searchText
         if (searchText.isBlank()) true
         else cur.currencyName.contains(other = searchText, ignoreCase = true)
                 || cur.currencyCode.contains(other = searchText, ignoreCase = true)
+    }
+
+    fun List<CurrencyEntity>?.sortedByOptions() : List<CurrencyEntity>? = this?.sortedBy { cur ->
+        when (screenState.value.sortOption) {
+            is SortOption.CurrencyCode -> cur.currencyCode
+            is SortOption.CurrencyName -> cur.currencyName
+        }.let { if (screenState.value.sortOption.ascending) it else it.reversed() }
     }
 }
--- common/currencies/impl/src/commonMain/kotlin/presentation/decompose/CurrencyListComponentImpl.kt
+++ common/currencies/impl/src/commonMain/kotlin/presentation/decompose/CurrencyListComponentImpl.kt
@@ -7,17 +7,15 @@ import data.repository.CurrenciesRepositoryNew
 import domain.model.CurrencyEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
@@ -29,7 +27,7 @@ import utils.SnackbarAction
 import utils.SnackbarController
 import utils.SnackbarEvent
 
-@OptIn(FlowPreview::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 class CurrencyListComponentImpl(
     componentContext: ComponentContext,
     private val onCurrencySelected: (CurrencyEntity) -> Unit,
@@ -37,29 +35,24 @@ class CurrencyListComponentImpl(
     private val componentScope: CoroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 ) : CurrencyListComponent, ComponentContext by componentContext, KoinComponent {
 
+    init {
+        lifecycle.doOnDestroy { componentScope.coroutineContext.cancelChildren() }
+    }
+
     private val newRepo: CurrenciesRepositoryNew by inject()
 
     private val _screenState = MutableStateFlow(CurrencyListScreenState())
     override val screenState: StateFlow<CurrencyListScreenState> = _screenState
         .onStart { fetchCurrencies() }
+        .transformLatest {
+            emit(it)
+            emit(it.copy(sortedData = it.data.filteredBySearch().sortedByOptions()))
+        }
         .stateIn(
             scope = componentScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = CurrencyListScreenState()
         )
-
-    init {
-        lifecycle.doOnDestroy { componentScope.coroutineContext.cancelChildren() }
-
-        CoroutineScope(Dispatchers.Default).launch {
-            _screenState
-                .distinctUntilChanged { old, new -> old.searchText == new.searchText }
-                .debounce(500)
-                .collectLatest {
-                    sortCurrenciesByName()
-                }
-        }
-    }
 
     override fun searchByName(searchText: String) {
         _screenState.update { it.copy(searchText = searchText) }
@@ -73,12 +66,10 @@ class CurrencyListComponentImpl(
         }
     }
 
-    private fun fetchCurrencies() {
+    private suspend fun fetchCurrencies() {
         _screenState.update { it.copy(loadingStatus = LoadingStatus.Loading) }
-        componentScope.launch {
-            newRepo.getCurrencies().handleResponse()
-            _screenState.update { it.copy(loadingStatus = LoadingStatus.Idle) }
-        }
+        newRepo.getCurrencies().handleResponse()
+        _screenState.update { it.copy(loadingStatus = LoadingStatus.Idle) }
     }
 
     override fun onCurrencyClick(currency: CurrencyEntity) = onCurrencySelected.invoke(currency)
@@ -89,16 +80,9 @@ class CurrencyListComponentImpl(
         _screenState.update { it.copy(sortOption = newSortOption) }
     }
 
-    private fun sortCurrenciesByName() {
-        _screenState.update { it.copy(sortedData = it.data.sortedBySearchText()) }
-    }
-
     private suspend fun Response<List<CurrencyEntity>>.handleResponse() = when (this) {
         is Response.Loading -> _screenState.update { it.copy(loadingStatus = LoadingStatus.Loading) }
-        is Response.Success -> {
-            _screenState.update { it.copy(data = data, sortedData = data) }
-            sortCurrenciesByName()
-        }
+        is Response.Success -> _screenState.update { it.copy(data = data, sortedData = data) }
         is Response.Failure -> {
             _screenState.update { it.copy(loadingStatus = LoadingStatus.Idle) }
             SnackbarController.sendEvent(
